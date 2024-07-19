31fe3c6cf72498a491f70031dedc43fe0f5b1806
Steven Gao <stevenruidigao@gmail.com>
2024-07-19 00:14:50 -0400

    Update init.ts

--- plugins/notification/guest-js/init.ts
+++ plugins/notification/guest-js/init.ts
@@ -6,6 +6,9 @@ import { invoke } from "@tauri-apps/api/core";
 import type { Options } from "./index";
 
 (function () {
+  // CHANGE (NEW): below(1): return
+  return;
+  
   let permissionSettable = false;
   let permissionValue = "default";
   // CHANGE: below(1): "granted" (temp disabled)
@@ -92,9 +95,6 @@ import type { Options } from "./index";
       permissionValue = v;
     },
   });
-
-  // CHANGE (NEW): below(1): return
-  return;
 
   // CHANGE: below(1): return
   return;
